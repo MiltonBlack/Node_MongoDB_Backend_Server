c05b89ac577faefe10f120b743f674c2abf5adbb
MiltonBlack <arziblack2@gmail.com>
2023-08-31 17:11:41 +0100

    successfully created a settings personnal and security route...

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,22 +9,22 @@ router.post("/signin", async (req, res) => {
     console.log(req.body);
     const user = await User.findOne({ email: req.body.email });
     try {
-        if(!user) {
-         res.status(401).json("User Email Not Found!!");
-        } else {    
-            console.log(user);        
+        if (!user) {
+            res.status(401).json("User Email Not Found!!");
+        } else {
+            console.log(user);
             const bytes = await CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
-            const realPassword = await  bytes.toString(CryptoJS.enc.Utf8);
-            
+            const realPassword = await bytes.toString(CryptoJS.enc.Utf8);
+
             realPassword !== req.body.password && res.status(401).json("Wrong Password or Username!!!");
             const accessToken = await jwt.sign({
                 id: user._id, email: user.email
-        },
-            process.env.SECRET_KEY, { expiresIn: "5d" }
-        );
-        const { password, ...info } = user._doc;
-        res.status(200).json({...info, accessToken});
-    }
+            },
+                process.env.SECRET_KEY, { expiresIn: "5d" }
+            );
+            const { password, ...info } = user._doc;
+            res.status(200).json({ ...info, accessToken });
+        }
     } catch (err) {
         res.status(500).json(err)
     }
@@ -47,7 +47,7 @@ router.post("/register", async (req, res) => {
 })
 
 
-router.get("/all", async (req, res)=> {
+router.get("/all", async (req, res) => {
     const all = await User.find();
     single = [all];
     try {
@@ -69,17 +69,71 @@ router.delete('/:id', (req, res) => {
     res.status(200).json({ message: `Delete Data ${req.params.id}` })
 })
 
+// Update User Plan
 router.put("/plan/:id", verify, async (req, res) => {
-    console.log(req.body)
-    console.log(req.params);
-    console.log(req.user);
     if (req.user.id === req.params.id) {
         try {
             const updateUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body, }
-                , {new: true});
+                , { new: true });
             res.status(200).json(updateUser);
         } catch (err) {
-            console.log(err);
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You can Only Update Your Account")
+    }
+});
+
+// Update Personnal settings
+router.put("/settings/personnal/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const updateUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body, }
+                , { new: true });
+            res.status(200).json(updateUser);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You can Only Update Your Account")
+    }
+});
+
+// Update Security Settings
+router.put("/settings/security/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id) {
+        var data;
+        if (req.body.password) {
+            req.body.password = CryptoJS.AES.encrypt(
+                req.body.password,
+                process.env.SECRET_KEY
+            ).toString();
+            
+            data = {
+                password: req.body.password
+            };
+        }
+        console.log(data);
+        try {
+            const updatePassword = await User.findByIdAndUpdate(req.params.id, { $set: data, }
+                , { new: true });
+            res.status(200).json(updatePassword.password);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You can Only Update Your Account")
+    }
+});
+
+// Update Account Settings
+router.put("/settings/account/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const updateUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body, }
+                , { new: true });
+            res.status(200).json(updateUser);
+        } catch (err) {
             res.status(500).json(err);
         }
     } else {
